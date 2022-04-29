e8ca404e4a1d18f45dbb155865ed361d6e9b6ad1
OpeDada <opedada@gmail.com>
2022-04-29 23:55:46 +0900

    add event listener to check JS file

--- source/assets/javascripts/projects.js
+++ source/assets/javascripts/projects.js
@@ -1,35 +1,39 @@
-function debounce(func, wait = 20, immediate = true) {
-  let timeout;
-  return function () {
-    let context = this,
-      args = arguments;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+document.addEventListener("turbolinks:load", () => {
+  alert("page has loaded!");
+});
 
-const sliderImages = document.querySelectorAll(".pic");
+// function debounce(func, wait = 20, immediate = true) {
+//   let timeout;
+//   return function () {
+//     let context = this,
+//       args = arguments;
+//     const later = function () {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     const callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
 
-function checkSlide() {
-  sliderImages.forEach((sliderImage) => {
-    // half way through the image
-    const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
-    // bottom of the image
-    const imageBottom = sliderImage.offsetTop + sliderImage.height;
-    const isHalfShown = slideInAt > sliderImage.offsetTop;
-    const isNotScrolledPast = window.scrollY < imageBottom;
-    if (isHalfShown && isNotScrolledPast) {
-      sliderImage.classList.add("active");
-    } else {
-      sliderImage.classList.remove("active");
-    }
-  });
-}
+// const sliderImages = document.querySelectorAll(".pic");
 
-window.addEventListener("scroll", debounce(checkSlide));
+// function checkSlide() {
+//   sliderImages.forEach((sliderImage) => {
+//     // half way through the image
+//     const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
+//     // bottom of the image
+//     const imageBottom = sliderImage.offsetTop + sliderImage.height;
+//     const isHalfShown = slideInAt > sliderImage.offsetTop;
+//     const isNotScrolledPast = window.scrollY < imageBottom;
+//     if (isHalfShown && isNotScrolledPast) {
+//       sliderImage.classList.add("active");
+//     } else {
+//       sliderImage.classList.remove("active");
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", debounce(checkSlide));
