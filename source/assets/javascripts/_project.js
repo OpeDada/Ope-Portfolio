dcd622892560578a529ff47a0be197bdc9e17e35
OpeDada <opedada@gmail.com>
2022-05-24 22:50:03 +0900

    add slide in function

--- source/assets/javascripts/_project.js
+++ source/assets/javascripts/_project.js
@@ -1,79 +1,42 @@
-const scrollElements = document.querySelectorAll(".js-scroll");
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this,
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
+const sliderImages = document.querySelectorAll(".slide-in");
 
-  return (
-    elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
-};
-
-const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el);
+function checkSlide(e) {
+  sliderImages.forEach((sliderImage) => {
+    // halfway through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
     }
+
+    console.log(slideInAt);
   });
-};
+}
 
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
+window.addEventListener("scroll", debounce(checkSlide));
+
+window.addEventListener("turbolinks:load", () => {
+  alert("page has loaded!");
 });
-
-// window.addEventListener("turbolinks:load", () => {
-//   alert("page has loaded!");
-// });
-
-// function debounce(func, wait = 20, immediate = true) {
-//   let timeout;
-//   return function () {
-//     let context = this,
-//       args = arguments;
-//     const later = function () {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     const callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// }
-
-// const sliderImages = document.querySelectorAll(".pic");
-
-// function checkSlide() {
-//   sliderImages.forEach((sliderImage) => {
-//     // half way through the image
-//     const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
-//     // bottom of the image
-//     const imageBottom = sliderImage.offsetTop + sliderImage.height;
-//     const isHalfShown = slideInAt > sliderImage.offsetTop;
-//     const isNotScrolledPast = window.scrollY < imageBottom;
-//     if (isHalfShown && isNotScrolledPast) {
-//       sliderImage.classList.add("active");
-//     } else {
-//       sliderImage.classList.remove("active");
-//     }
-//   });
-// }
-
-// window.addEventListener("scroll", debounce(checkSlide));
